0b598f790ae4914b466a7772ad97de2d46ce3fce
ShuvoAsif <asif.shuvo.learning@gmail.com>
2022-09-29 19:36:22 +0600

    break time

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './List.css';
 
 const List = () => {
+    const [breakTime, setBreakTime] = useState([0]);
+
+
+    const handleSetBreakTime = (time) => {
+        const selectedBreakTime = time;
+        const newBreakTime = [selectedBreakTime];
+        setBreakTime(newBreakTime);
+        console.log(selectedBreakTime)
+    }
+
+
     return (
         <div className='list'>
             <h3>Asif Ibne Kamal</h3>
@@ -20,17 +31,17 @@ const List = () => {
             <div>
                 <h3>Add a Break</h3>
                 <div className='break'>
-                    <button className='btn-break'><h6>10<small>S</small></h6></button>
-                    <button className='btn-break'><h6>20<small>S</small></h6></button>
-                    <button className='btn-break'><h6>30<small>S</small></h6></button>
-                    <button className='btn-break'><h6>40<small>S</small></h6></button>
-                    <button className='btn-break'><h6>50<small>S</small></h6></button>
+                    <button onClick={() => handleSetBreakTime(10)} className='btn-break'><h6><small>10</small>s</h6></button>
+                    <button onClick={() => handleSetBreakTime(20)} className='btn-break'><h6><small>20</small>s</h6></button>
+                    <button onClick={() => handleSetBreakTime(30)} className='btn-break'><h6><small>30</small>s</h6></button>
+                    <button onClick={() => handleSetBreakTime(40)} className='btn-break'><h6><small>40</small>s</h6></button>
+                    <button onClick={() => handleSetBreakTime(50)} className='btn-break'><h6><small>50</small>s</h6></button>
                 </div>
             </div>
             <div>
                 <h3>Exercise Details</h3>
                 <h5 className='time'>Exercise Time: <small></small>s</h5>
-                <h5 className='time'>Break Time: <small></small>s</h5>
+                <h5 className='time'>Break Time: {breakTime} S</h5>
             </div>
         </div>
     );
